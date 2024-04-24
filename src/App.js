2b64ef54c820b80a481e16e8f088aa952f7007d5
JinY <wlsdud6221@naver.com>
2024-04-25 02:53:07 +0900

    ge1 오류 디버깅

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { styled } from 'styled-components';
 import './App.css';
 import GlobalStyle from './style/GlobalStyle';
 import './pages/LoginPage';
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage';
 import SplashPage from './pages/SplashPage';
-
 
 const S = {
   Frame: styled.div`
@@ -43,7 +42,7 @@ function App() {
       <GlobalStyle />
       <S.Background />
       <S.Frame>
-        <LoginPage/>
+        <LoginPage />
       </S.Frame>
     </>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,78 +1,75 @@
-import React from "react";
+import React from 'react';
 import { styled } from 'styled-components';
 
-
 const LoginContainer = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const LogoImage = styled.img`
-    position: absolute;
-    top: 168px;
+  position: absolute;
+  top: 168px;
 `;
 
 const LoginInput = styled.input`
-    width: 368px;
-    height: 54px;
-    background: #F0F0F0;
-    border-radius: 8px;
-    position: absolute;
-    padding: 15px;
+  width: 368px;
+  height: 54px;
+  background: #f0f0f0;
+  border-radius: 8px;
+  position: absolute;
+  padding: 15px;
 `;
 
-const LoginButton = styled.button `
-    width: 368px;
-    height: 54px;
-    border-radius:8px;
-    position: absolute;
-    background: #A7968E;
-    font-size: 20px
-    font-weight: 600;
-    color: white;
-
+const LoginButton = styled.button`
+  width: 368px;
+  height: 54px;
+  border-radius: 8px;
+  position: absolute;
+  background: #a7968e;
+  font-size: 20px;
+  font-weight: 600;
+  color: white;
 `;
 
 const LoginP = styled.p`
-    width: 260px;
-    height: 24px;
-    position: absolute;
-    font-size:18px;
-    top: 724px;
-    text-align: center;
+  width: 260px;
+  height: 24px;
+  position: absolute;
+  font-size: 18px;
+  top: 724px;
+  text-align: center;
 `;
 
 const LoginLink = styled.a`
-    color: #3F8CFF;
-    font-size:18px;
-
-
+  color: #3f8cff;
+  font-size: 18px;
 `;
 
-function LoginPage(){
-    return(
-        <>
-        <LoginContainer>
-        <LogoImage src= {process.env.PUBLIC_URL + 'mstile-150x150.png'}/>
-            <LoginInput 
-                type="email"
-                placeholder="이메일을 입력해주세요"
-                style={{top:'472px'}}
-                />
-            <LoginInput 
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-                style={{top:'536px'}}/>
-            <LoginButton
-                style={{top:'610px'}}
-            >로그인하기</LoginButton>
-            <LoginP> 회원이 아니신가요? <LoginLink>회원가입하기 {'>'}</LoginLink></LoginP>
-        </LoginContainer>
-        </>
-    );
+function LoginPage() {
+  return (
+    <>
+      <LoginContainer>
+        <LogoImage src={process.env.PUBLIC_URL + 'mstile-150x150.png'} />
+        <LoginInput
+          type="email"
+          placeholder="이메일을 입력해주세요"
+          style={{ top: '472px' }}
+        />
+        <LoginInput
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          style={{ top: '536px' }}
+        />
+        <LoginButton style={{ top: '610px' }}>로그인하기</LoginButton>
+        <LoginP>
+          {' '}
+          회원이 아니신가요? <LoginLink>회원가입하기 {'>'}</LoginLink>
+        </LoginP>
+      </LoginContainer>
+    </>
+  );
 }
 
 export default LoginPage;
