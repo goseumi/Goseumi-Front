358edf71a819c10be9f89b57a6a7983d0fa9a977
yungan9 <geyun105@naver.com>
2024-05-06 00:00:19 -0700

    navbar component

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { styled } from 'styled-components';
+import {ReactComponent as Nav1} from "../../assets/img/bottomNav-1.svg";
+import {ReactComponent as Nav2} from "../../assets/img/bottomNav-2.svg";
+import {ReactComponent as Nav3} from "../../assets/img/bottomNav-3.svg";
+import {ReactComponent as Nav4} from "../../assets/img/bottomNav-4.svg";
+import {ReactComponent as Nav5} from "../../assets/img/bottomNav-5.svg";
+
+
+const NavContainer = styled.div`
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+
+`;
+
+const Tap = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 20%;
+    height: 94px;
+    line-height: 30px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 500;
+    color: #A6A6A6;
+    border-top: 1px solid #F0F0F0;
+
+`;
+
+function Nav({ icon: Icon, text }) {
+    return (
+        <Tap>
+            <Icon width={24} height={24} fill="none" />
+            {text}
+        </Tap>
+    );
+}
+
+function NavBar() {
+    return (
+        <NavContainer>
+            <Nav icon={Nav1} text="홈" />
+            <Nav icon={Nav2} text="게시판" />
+            <Nav icon={Nav3} text="시간표" />
+            <Nav icon={Nav4} text="DM" />
+            <Nav icon={Nav5} text="문방구" />
+        </NavContainer>
+    );
+}
+
+export default NavBar;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import NavBar from "../components/common/NavBar";
+
+function MainPage() {
+    return(
+    <>
+    <div>dkdh</div>
+        <NavBar/>
+    </>);
+}
+
+export default MainPage;
