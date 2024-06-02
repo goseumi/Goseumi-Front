764b1cd1d0c4c207fe339cd522281377a6ff6e20
JinY <wlsdud6221@naver.com>
2024-06-03 03:35:15 +0900

    DM UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,54 @@
+// import React from 'react';
+// import { styled } from 'styled-components';
+// import './App.css';
+// import GlobalStyle from './style/GlobalStyle';
+// import './pages/LoginPage';
+// import LoginPage from './pages/LoginPage';
+// import SplashPage from './pages/SplashPage';
+
+// const S = {
+//   Frame: styled.div`
+//     max-width: 428px;
+//     height: 100vh;
+//     position: relative;
+//     margin: 0 auto;
+//     background-color: #ffffff;
+//     box-shadow: -5px -5px 30px 5px black;
+//     overflow: auto;
+//   `,
+
+//   Background: styled.div`
+//     width: 100vw;
+//     height: 100%;
+//     background: #f7f7f7;
+//     position: absolute;
+//     font-size: 80px;
+//     line-height: 70px;
+//   `,
+//   Test: styled.div`
+//     //테스트코드
+//     max-width: 428px;
+//     height: 100vh;
+//     border: 1px solid red;
+//     background: #ffffff;
+//     display: flex;
+//     position: relative;
+//   `,
+// };
+
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyle />
+//       <S.Background />
+//       <S.Frame>
+//         <LoginPage />
+//       </S.Frame>
+//     </>
+//   );
+// }
+
+// export default App;
 import React from 'react';
 import { styled } from 'styled-components';
 import './App.css';
@@ -5,7 +56,8 @@ import GlobalStyle from './style/GlobalStyle';
 import './pages/LoginPage';
 import LoginPage from './pages/LoginPage';
 import SplashPage from './pages/SplashPage';
-
+import MessagePage from './pages/Message/MessagePage';
+import MessageWritePage from './pages/Message/MessageWritePage';
 
 const S = {
   Frame: styled.div`
@@ -43,7 +95,8 @@ function App() {
       <GlobalStyle />
       <S.Background />
       <S.Frame>
-        <LoginPage />
+        {/* <MessagePage /> */}
+        <MessageWritePage />
       </S.Frame>
     </>
   );
