c5ba2ee15d71c9f20c43cf3a14e66758f847be3d
JinY <wlsdud6221@naver.com>
2024-06-09 01:39:18 +0900

    recoil modal Atom 설정

---
+++ src/lib/recoil/modalAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const timeSetAtom = atom({
+  key: 'timeSetAtom',
+  default: false,
+});
+
+export const GradeSetAtom = atom({
+  key: 'GradeSetAtom',
+  default: false,
+});
