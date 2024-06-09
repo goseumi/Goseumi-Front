d93fb2e6bafdba95cbbb421abfbeb9f9ce3a2c8d
JinY <wlsdud6221@naver.com>
2024-06-10 00:44:18 +0900

    main App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,3 @@
-// import React from 'react';
-// import { styled } from 'styled-components';
-// import './App.css';
-// import GlobalStyle from './style/GlobalStyle';
-// import './pages/LoginPage';
-// import LoginPage from './pages/LoginPage';
-// import SplashPage from './pages/SplashPage';
-
-// const S = {
-//   Frame: styled.div`
-//     max-width: 428px;
-//     height: 100vh;
-//     position: relative;
-//     margin: 0 auto;
-//     background-color: #ffffff;
-//     box-shadow: -5px -5px 30px 5px black;
-//     overflow: auto;
-//   `,
-
-//   Background: styled.div`
-//     width: 100vw;
-//     height: 100%;
-//     background: #f7f7f7;
-//     position: absolute;
-//     font-size: 80px;
-//     line-height: 70px;
-//   `,
-//   Test: styled.div`
-//     //테스트코드
-//     max-width: 428px;
-//     height: 100vh;
-//     border: 1px solid red;
-//     background: #ffffff;
-//     display: flex;
-//     position: relative;
-//   `,
-// };
-
-// function App() {
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <S.Background />
-//       <S.Frame>
-//         <LoginPage />
-//       </S.Frame>
-//     </>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { styled } from 'styled-components';
 import './App.css';
@@ -56,7 +5,6 @@ import GlobalStyle from './style/GlobalStyle';
 import './pages/LoginPage';
 import LoginPage from './pages/LoginPage';
 import SplashPage from './pages/SplashPage';
-import TimeTablePage from './pages/TimeTablePage';
 
 const S = {
   Frame: styled.div`
@@ -94,8 +42,7 @@ function App() {
       <GlobalStyle />
       <S.Background />
       <S.Frame>
-        <TimeTablePage />
-        {/* <SplashPage /> */}
+        <LoginPage />
       </S.Frame>
     </>
   );
