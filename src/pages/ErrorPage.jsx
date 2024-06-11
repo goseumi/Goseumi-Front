f8c759ca6bfb8b31c81f7a0c8aaee2b8aa809378
JinY <wlsdud6221@naver.com>
2024-06-12 02:20:26 +0900

    에러페이지 구현

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -12,6 +12,31 @@ const s = {
     &::-webkit-scrollbar {
       display: none;
     }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  content: styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  title: styled.p`
+    font-size: 25px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  `,
+  text: styled.p`
+    font-size: 14px;
+  `,
+  btn: styled.button`
+    width: 100%;
+    height: 50px;
+    background-color: #a7968e;
+    border-radius: 10px;
+    margin: 30px 0;
+    color: white;
   `,
 };
 
@@ -19,8 +44,13 @@ const ErrorPage = () => {
   return (
     <>
       <s.Frame>
-        에러페이지
-        <FormButton />
+        <s.content>
+          <s.title>다시 확인해주세요!</s.title>
+          <s.text>
+            해당 경로는 잘못된 접근이거나 존재하지 않는 페이지 입니다.
+          </s.text>
+          <s.btn>홈 화면으로</s.btn>
+        </s.content>
       </s.Frame>
     </>
   );
