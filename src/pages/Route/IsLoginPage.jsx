b2b99cb0957cab8214763502c546eaacf1a41a80
JinY <wlsdud6221@naver.com>
2024-06-13 03:00:36 +0900

    로그인 유저 판별

--- src/pages/Route/IsLoginPage.jsx
+++ src/pages/Route/IsLoginPage.jsx
@@ -3,14 +3,19 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { isLogin } from '../../lib/recoil/isLoginAtom';
 
-export const IsLoginOn = () => {
+export const PrivateRoute = () => {
   const checkLogin = useRecoilValue(isLogin);
-  console.log('로그인했다');
-  return checkLogin ? <Outlet /> : <Navigate to={'/home'} />;
+
+  if (!checkLogin) {
+    alert('로그인 후 접근 가능합니다.');
+  }
+  return checkLogin ? <Outlet /> : <Navigate to={'/login'} />;
 };
 
-export const IsLoginOff = () => {
+export const PublicRoute = () => {
   const checkLogin = useRecoilValue(isLogin);
-  console.log('로그인안했다');
-  return checkLogin ? <Navigate to={'/dm'} /> : <Outlet />;
+  if (checkLogin) {
+    alert('비정상적인 접근 : 로그인유저가 public 접근');
+  }
+  return checkLogin ? <Navigate to={'*'} /> : <Outlet />;
 };
