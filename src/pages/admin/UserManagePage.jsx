77ba30ba2df8cf41ab82816edd95d7581c29ed4d
JinY <wlsdud6221@naver.com>
2024-06-14 04:21:52 +0900

    관리자 마이페이지

---
+++ src/pages/admin/UserManagePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import FormHeader from './../../components/common/FormHeader';
+import MyPageList from './../../components/myPage/MyPageList';
+import NewUser from '../../components/admin/NewUser';
+
+const s = {
+  Frame: styled.div`
+    max-width: 428px;
+    height: 100vh;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `,
+};
+
+const UserManagePage = () => {
+  return (
+    <>
+      <s.Frame>
+        <FormHeader text="회원 관리" />
+        <NewUser />
+      </s.Frame>
+    </>
+  );
+};
+
+export default UserManagePage;
