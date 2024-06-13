36b9956fc951ecd6f9148bef99f37765866968ac
JinY <wlsdud6221@naver.com>
2024-06-13 15:59:20 +0900

    header fixed 설정

--- src/components/common/MainHeader.jsx
+++ src/components/common/MainHeader.jsx
@@ -3,42 +3,51 @@ import styled from 'styled-components';
 import { ReactComponent as icons1 } from '../../assets/img/icons-1.svg';
 import { ReactComponent as icons2 } from '../../assets/img/icons-2.svg';
 import { ReactComponent as icons3 } from '../../assets/img/icons-3.svg';
-import logo from '../../assets/img/mainLogo2.png';
+import logo from '../../assets/img/HeadLogo.png';
 import NavBar from './NavBar';
 import { useNavigate } from 'react-router-dom';
 
-const MainHeaderContainer = styled.div`
-  height: 60px;
-  border-bottom: 1px solid #f0f0f0;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
+const s = {
+  fixedDiv: styled.div`
+    width: 100%;
+    max-width: 428px;
+    position: fixed;
+    background-color: #ffffff;
+  `,
+  MainHeaderContainer: styled.div`
+    height: 60px;
+    border-bottom: 1px solid #f0f0f0;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
 
-const Logo = styled.div`
-  width: 150px;
-  height: 34px;
-  /* border-radius: 50%; */
-  /* background-color: #f0f0f0; */
-  margin-left: 30px;
-  background-image: url(${logo});
-  background-position: center center;
-  background-size: cover;
-  background-repeat: no-repeat;
-`;
+  Logo: styled.div`
+    width: 150px;
+    height: 40px;
+    /* border-radius: 50%; */
+    /* background-color: #f0f0f0; */
+    margin: 10px 0 0 20px;
+    background-image: url(${logo});
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    /* border: 1px solid red; */
+  `,
 
-const IconContainer = styled.div`
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
+  IconContainer: styled.div`
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  `,
 
-const Tab = styled.div`
-  margin-right: 22px;
-  cursor: pointer;
-`;
+  Tab: styled.div`
+    margin-right: 22px;
+    cursor: pointer;
+  `,
+};
 
 function Icons({ icon: Icon, path }) {
   const navigate = useNavigate();
@@ -47,28 +56,28 @@ function Icons({ icon: Icon, path }) {
     navigate(path);
   };
   return (
-    <Tab
+    <s.Tab
       onClick={(e) => {
         movePage(path);
       }}
     >
       <Icon width={24} height={24} fill="none" />
-    </Tab>
+    </s.Tab>
   );
 }
 
 function MainHeader() {
   return (
-    <>
-      <MainHeaderContainer>
-        <Logo />
-        <IconContainer>
+    <s.fixedDiv>
+      <s.MainHeaderContainer>
+        <s.Logo />
+        <s.IconContainer>
           <Icons icon={icons1} path="/" />
           <Icons icon={icons2} path="/" />
           <Icons icon={icons3} path="/mypage" />
-        </IconContainer>
-      </MainHeaderContainer>
-    </>
+        </s.IconContainer>
+      </s.MainHeaderContainer>
+    </s.fixedDiv>
   );
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,12 +6,16 @@ import MainHeader from '../components/common/MainHeader';
 import HalfBoard from '../components/common/HalfBoard';
 import MainBoard from '../components/common/MainBoard';
 import { useLocation } from 'react-router-dom';
-import icon0 from '../assets/img/mainLogo_back.png';
+import icon0 from '../assets/img/mainLogo.png';
 import icon1 from '../assets/img/neis.png';
 import icon2 from '../assets/img/jinhak.jpg';
 import icon3 from '../assets/img/ebs.png';
 
 const s = {
+  MainContent: styled.main`
+    width: 100%;
+    padding-top: 60px;
+  `,
   SiteContainer: styled.div`
     overflow: hidden;
     display: flex;
@@ -37,30 +41,33 @@ const MainPage = () => {
   return (
     <>
       <MainHeader />
-      <s.SchoolName>공주고등학교</s.SchoolName>
-      <s.SiteContainer>
-        <SiteTab
-          icon={icon0}
-          href="http://www.kongjugo.cnehs.kr/main.do"
-          text="홈페이지"
-        />
-        <SiteTab
-          icon={icon1}
-          href="https://www.neis.go.kr/nxuiPortal/index.html"
-          text="나이스"
-        />
-        <SiteTab icon={icon2} href="https://www.jinhak.com/" text="진학사" />
-        <SiteTab
-          icon={icon3}
-          href="https://www.ebsi.co.kr/ebs/pot/poti/main.ebs"
-          text="EBS"
-        />
-      </s.SiteContainer>
-      <s.BoardContainer>
-        <HalfBoard text="오늘의 급식" />
-        <HalfBoard text="학사 일정" />
-      </s.BoardContainer>
-      <MainBoard text="커뮤니티 목록" />
+      <s.MainContent>
+        <s.SchoolName>공주고등학교</s.SchoolName>
+        <s.SiteContainer>
+          <SiteTab
+            icon={icon0}
+            href="http://www.kongjugo.cnehs.kr/main.do"
+            text="홈페이지"
+          />
+          <SiteTab
+            icon={icon1}
+            href="https://www.neis.go.kr/nxuiPortal/index.html"
+            text="나이스"
+          />
+          <SiteTab icon={icon2} href="https://www.jinhak.com/" text="진학사" />
+          <SiteTab
+            icon={icon3}
+            href="https://www.ebsi.co.kr/ebs/pot/poti/main.ebs"
+            text="EBS"
+          />
+        </s.SiteContainer>
+        <s.BoardContainer>
+          <HalfBoard text="오늘의 급식" />
+          <HalfBoard text="학사 일정" />
+        </s.BoardContainer>
+        <MainBoard text="커뮤니티 목록" />
+      </s.MainContent>
+
       <NavBar />
     </>
   );
