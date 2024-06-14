25a8fb310f85d1e99602de15e7b8daf1be526c0c
JinY <wlsdud6221@naver.com>
2024-06-14 20:37:28 +0900

    전역 colors 설정

---
+++ src/style/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    // ${({theme}) => theme.colors.컬러이름}
+    Gray: '#f1f3f5',
+    mainLine: '#a6a6a6',
+    main: '#A7968E',
+    sub: '#FFC4C4',
+    negative: '#FF6E65',
+    positive: '#3F8CFF',
+  },
+};
