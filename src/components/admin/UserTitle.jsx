116dbc452d213cd530237890ca1e205e0a948e87
JinY <wlsdud6221@naver.com>
2024-06-15 01:29:38 +0900

    유저 관련 컴포넌트 생성

---
+++ src/components/admin/UserTitle.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const s = {
+  titleDiv: styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 5px;
+  `,
+  title: styled.p`
+    font-size: 15px;
+    font-weight: bold;
+  `,
+  count: styled.p`
+    font-size: 15px;
+    font-weight: bold;
+  `,
+};
+
+const UserTitle = ({ title, count }) => {
+  return (
+    <s.titleDiv>
+      <s.title>{title}</s.title>
+      <s.count>{count} 명</s.count>
+    </s.titleDiv>
+  );
+};
+
+export default UserTitle;
