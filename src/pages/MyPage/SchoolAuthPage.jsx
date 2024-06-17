983e09b88c3e6542150d85b23951f1c51026976c
JinY <wlsdud6221@naver.com>
2024-06-18 03:32:19 +0900

    학교 인증 페이지

--- src/pages/MyPage/SchoolAuthPage.jsx
+++ src/pages/MyPage/SchoolAuthPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as icon } from '../../assets/img/Logo.svg';
 import { ReactComponent as search } from '../../assets/img/icons-1.svg';
@@ -9,6 +9,7 @@ import SchoolSearchModal from '../../components/modal/SchoolSearchModal';
 import { useRecoilState } from 'recoil';
 import { SchoolSetAtom } from '../../lib/recoil/modalAtom';
 import FormButton from './../../components/common/FormButton';
+import Icon1 from '../../assets/img/imgUpload_icon.png';
 
 const s = {
   Frame: styled.div`
@@ -24,7 +25,7 @@ const s = {
   `,
   content: styled.div`
     width: 100%;
-    height: 70vh;
+    /* height: 70vh; */
     border: 1px solid red;
     display: flex;
     flex-direction: column;
@@ -71,27 +72,60 @@ const s = {
     display: inline;
     cursor: pointer;
   `,
-  dataDiv: styled.div`
-    width: 90%;
-    height: 300px;
-    border: 1px solid blue;
-  `,
   btnDiv: styled.div`
     width: 90%;
+  `,
+  //이미지
+  ImgBox: styled.div`
+    width: 90%;
+    height: 200px;
+    border: 1px solid ${({ theme }) => theme.colors.mainLine};
+    margin: 0 auto;
+    cursor: pointer;
+  `,
+  ImgIcon: styled.img`
+    width: 100%;
+    height: 200px;
   `,
 };
 
 const SchoolAuthPage = () => {
   const [open, setOpen] = useRecoilState(SchoolSetAtom);
-
+  const [selectedImage, setSelectedImage] = useState();
+  const [imgFile, setImgFile] = useState('');
+  const fileInputRef = useRef(null); // useRef를 생성
   const search = () => {
     setOpen(!open);
   };
+
+  // 이미지 선택 시 호출되는 핸들러
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImgFile(file);
+    if (file) {
+      // 선택한 파일을 미리보기로 표시
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setSelectedImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // const getDamage = async () => {
+  //   // 이미지 서버로 보내는 작업
+  //   try {
+  //     const response = await ImgUploadApi(imgFile, DamageSelect);
+  //     setData(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
   return (
     <>
       <s.Frame>
-        <s.sLogo width={100} height={100} fill="none" />
         <s.content>
+          <s.sLogo width={100} height={100} fill="none" />
           <s.BoldTitle>잠깐!!</s.BoldTitle>
           <s.Text>
             고슴이는 회원님의 학교 인증 및 승인이 있어야 이용 가능합니다. <br />
@@ -104,12 +138,12 @@ const SchoolAuthPage = () => {
             일괄 파기됩니다.
           </s.BoldText>
           <s.searchDiv>
-            <s.condition>
-              <option value="" selected disabled={true}>
+            <s.condition defaultValue={''}>
+              <option value="" disabled={true}>
                 검색조건
               </option>
-              <option value="1">학교명</option>
-              <option value="1">주소</option>
+              <option value="school">학교명</option>
+              <option value="addr">주소</option>
             </s.condition>
             <s.input
               id="nick"
@@ -119,7 +153,23 @@ const SchoolAuthPage = () => {
             />
             <s.sIcon width={24} height={24} fill="none" onClick={search} />
           </s.searchDiv>
-          <s.dataDiv></s.dataDiv>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            style={{ display: 'none' }}
+            ref={fileInputRef} // ref를 useRef로 연결
+          />
+          <s.ImgBox onClick={() => fileInputRef.current.click()}>
+            {imgFile === '' ? (
+              <s.ImgIcon
+                style={{ width: '200px', margin: '0 auto' }}
+                src={Icon1}
+              />
+            ) : (
+              <s.ImgIcon src={selectedImage} />
+            )}
+          </s.ImgBox>
           <s.btnDiv>
             <FormButton type="add" onClick={() => alert('등록')} />
           </s.btnDiv>
