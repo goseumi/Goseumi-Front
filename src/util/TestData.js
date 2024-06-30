a4eeb2f3c440b44257abffc157db2b03f1c0b2b8
JinY <wlsdud6221@naver.com>
2024-07-01 02:44:30 +0900

    내신계산 모의데이터

---
+++ src/util/TestData.js
@@ -0,0 +1,194 @@
+export const grade = [
+  [
+    {
+      name: '국어',
+      score: 90,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 65,
+      grade: 5,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 85,
+      grade: 3,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 40,
+      grade: 6,
+      import: true,
+    },
+    {
+      name: '과학',
+      score: 60,
+      grade: 5,
+      import: true,
+    },
+  ],
+  [
+    {
+      name: '국어',
+      score: 82,
+      grade: 3,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 61,
+      grade: 5,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 71,
+      grade: 4,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 78,
+      grade: 5,
+      import: true,
+    },
+    {
+      name: '과학',
+      score: 80,
+      grade: 4,
+      import: true,
+    },
+  ],
+  [
+    {
+      name: '국어',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 70,
+      grade: 4,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 88,
+      grade: 2,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 61,
+      grade: 4,
+      import: false,
+    },
+    {
+      name: '과학',
+      score: 21,
+      grade: 9,
+      import: false,
+    },
+  ],
+  [
+    {
+      name: '국어',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '과학',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+  ],
+  [
+    {
+      name: '국어',
+      score: 80,
+      grade: 4,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 88,
+      grade: 2,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 94,
+      grade: 2,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 78,
+      grade: 4,
+      import: true,
+    },
+    {
+      name: '과학',
+      score: 78,
+      grade: 4,
+      import: true,
+    },
+  ],
+  [
+    {
+      name: '국어',
+      score: 90,
+      grade: 2,
+      import: true,
+    },
+    {
+      name: '영어',
+      score: 82,
+      grade: 3,
+      import: true,
+    },
+    {
+      name: '수학',
+      score: 82,
+      grade: 3,
+      import: true,
+    },
+    {
+      name: '사회',
+      score: 100,
+      grade: 1,
+      import: true,
+    },
+    {
+      name: '물리',
+      score: 60,
+      grade: 4,
+      import: true,
+    },
+  ],
+];
