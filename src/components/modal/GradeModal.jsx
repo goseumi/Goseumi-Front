b71616fd26733eeefaba05e0a547268f7c6258c6
JinY <wlsdud6221@naver.com>
2024-07-02 01:35:29 +0900

    내신 및 주요 등급 평균 계산

--- src/components/modal/GradeModal.jsx
+++ src/components/modal/GradeModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import ReactModal from 'react-modal';
 import '../../style/modal.css';
@@ -52,11 +52,10 @@ const s = {
   `,
 };
 
-const GradeModal = () => {
+const GradeModal = (props) => {
   const [open, setOpen] = useRecoilState(GradeSetAtom);
   const [report, setReport] = useState([]);
   const [important, setImportant] = useState([]);
-  const [gradeState, setGradeState] = useRecoilState(GradeDataState);
   const [grades, setGrades] = useState({
     grade: '1학년 1학기',
     idx: 0,
@@ -76,9 +75,7 @@ const GradeModal = () => {
     console.log(report[grades.idx]);
   };
 
-  const handleSaveData = (data) => {
-    console.log(data);
-  };
+  const myRef = useRef();
 
   const CalReport = [];
   const CalImportant = [];
@@ -100,8 +97,22 @@ const GradeModal = () => {
     }
     setReport(CalReport);
     setImportant(CalImportant);
+
+    const filterReport = CalReport.filter((n) => n !== 0);
+    const filterImport = CalImportant.filter((n) => n !== 0);
+
+    const avgReport =
+      filterReport.length > 0 ? filterReport.reduce((acc, curr) => acc + curr, 0) / filterReport.length : 0;
+    const avgImport =
+      filterImport.length > 0 ? filterImport.reduce((acc, curr) => acc + curr, 0) / filterImport.length : 0;
+
+    console.log('평균 내신:', Math.round(avgReport * 10) / 10);
+    console.log('평균 주요:', Math.round(avgImport * 10) / 10);
     console.log('내신 : ' + report);
     console.log('주요 : ' + important);
+
+    props.setReport(Math.round(avgReport * 10) / 10);
+    props.setImportant(Math.round(avgImport * 10) / 10);
   }, []);
 
   return (
@@ -119,13 +130,13 @@ const GradeModal = () => {
           <s.mainInfo>
             <s.title>{grades.grade}</s.title>
             <s.subtitle>
-              내신 <s.boldText>{report[grades.idx]}등급</s.boldText>
-              주요 과목 <s.boldText>{important[grades.idx]}등급</s.boldText>
+              내신 : <s.boldText>{report[grades.idx]} 등급</s.boldText>
+              주요 과목 : <s.boldText>{important[grades.idx]} 등급</s.boldText>
             </s.subtitle>
           </s.mainInfo>
-          <s.saveBtn onClick={handleSaveData}>저장하기</s.saveBtn>
+          <s.saveBtn onClick={() => myRef.current.handleChildSaveData()}>저장하기</s.saveBtn>
         </s.info>
-        <GradeTable datas={grade[grades.idx]} onSaveData={handleSaveData} />
+        <GradeTable datas={grade[grades.idx]} ref={myRef} />
       </s.content>
     </ReactModal>
   );
