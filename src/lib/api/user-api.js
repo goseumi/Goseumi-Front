dfffefcda50143da7b75fc057c93d4036907953c
JinY <wlsdud6221@naver.com>
2024-07-04 23:44:09 +0900

    엔드포인트 수정

--- src/lib/api/user-api.js
+++ src/lib/api/user-api.js
@@ -4,8 +4,8 @@ const local = localAxios();
 const join = registAxios();
 
 export const login = async (param, success, fail) => {
-  await local.post(`/member/signup`, param).then(success).catch(fail);
+  await local.post(`/login`, param).then(success).catch(fail);
 };
 export const regist = async (param, success, fail) => {
-  await join.post(`/login`, param).then(success).catch(fail);
+  await join.post(`/api/member/signup`, param).then(success).catch(fail);
 };
