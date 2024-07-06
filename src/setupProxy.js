206724eb9a886e3de914c19a4faa34746869572e
JinY <wlsdud6221@naver.com>
2024-07-07 02:22:25 +0900

    user, admin 프록시 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,4 +11,24 @@ module.exports = function (app) {
       },
     }),
   );
+  app.use(
+    '/admin',
+    createProxyMiddleware({
+      target: `${process.env.REACT_APP_BASE_URL}`, //타겟이 되는 api url
+      changeOrigin: true, // 서버 구성에 따른 호스트 헤더 변경 여부 설정
+      pathRewrite: {
+        '^/admin': '/api/admin',
+      },
+    }),
+  );
+  app.use(
+    '/user',
+    createProxyMiddleware({
+      target: `${process.env.REACT_APP_BASE_URL}`, //타겟이 되는 api url
+      changeOrigin: true, // 서버 구성에 따른 호스트 헤더 변경 여부 설정
+      pathRewrite: {
+        '^/user': '/api',
+      },
+    }),
+  );
 };
