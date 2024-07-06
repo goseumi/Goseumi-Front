d73f6ddc4c20e490f8ae538aa4d357d1c6657cd5
JinY <wlsdud6221@naver.com>
2024-07-07 02:23:03 +0900

    local, user, admin으로 세분화

--- src/util/axios-setting.js
+++ src/util/axios-setting.js
@@ -12,10 +12,21 @@ export const localAxios = () => {
   return instance;
 };
 
-export const registAxios = () => {
-  //api단이 빠짐
+export const userAxios = () => {
   const instance = axios.create({
-    baseURL: '/proxy',
+    baseURL: '/user',
+    withCredentials: true,
+  });
+
+  instance.defaults.headers.common['Authorization'] = '';
+  instance.defaults.headers.post['Content-Type'] = 'application/json';
+  instance.defaults.headers.patch['Content-Type'] = 'application/json';
+  return instance;
+};
+
+export const adminAxios = () => {
+  const instance = axios.create({
+    baseURL: '/admin',
     withCredentials: true,
   });
 
