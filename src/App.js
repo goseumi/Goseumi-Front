4e9a80f17baaa01af5c640132666d1047e137530
JinY <wlsdud6221@naver.com>
2024-09-22 23:53:08 +0900

    [FE] docs : App.js 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,54 +1,3 @@
-// import React from 'react';
-// import { styled } from 'styled-components';
-// import './App.css';
-// import GlobalStyle from './style/GlobalStyle';
-// import './pages/LoginPage';
-// import LoginPage from './pages/LoginPage';
-// import SplashPage from './pages/SplashPage';
-
-// const S = {
-//   Frame: styled.div`
-//     max-width: 428px;
-//     height: 100vh;
-//     position: relative;
-//     margin: 0 auto;
-//     background-color: #ffffff;
-//     box-shadow: -5px -5px 30px 5px black;
-//     overflow: auto;
-//   `,
-
-//   Background: styled.div`
-//     width: 100vw;
-//     height: 100%;
-//     background: #f7f7f7;
-//     position: absolute;
-//     font-size: 80px;
-//     line-height: 70px;
-//   `,
-//   Test: styled.div`
-//     //테스트코드
-//     max-width: 428px;
-//     height: 100vh;
-//     border: 1px solid red;
-//     background: #ffffff;
-//     display: flex;
-//     position: relative;
-//   `,
-// };
-
-// function App() {
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <S.Background />
-//       <S.Frame>
-//         <LoginPage/>
-//       </S.Frame>
-//     </>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { ThemeProvider, styled } from 'styled-components';
 import './App.css';
@@ -66,8 +15,8 @@ import MessageWritePage from './pages/Message/MessageWritePage';
 import MListBox from './components/message/MListBox';
 import MRead from './components/message/MRead';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import BoardList from './pages/Board/BoardList';
-import BoardInput from './pages/Board/BoardInput';
+import BoardListPage from './pages/Board/BoardListPage';
+import BoardWritePage from './pages/Board/BoardWritePage';
 import BoardDetailPage from './pages/Board/BoardDetailPage';
 import ErrorPage from './pages/ErrorPage';
 import IsLoginPage, { AdminRoute, PrivateRoute, PublicRoute } from './pages/Route/IsLoginPage';
@@ -141,8 +90,8 @@ function App() {
                   <Route path="read" element={<MRead />} />
                   <Route path="send" element={<MessageWritePage />} />
                 </Route>
-                <Route path="/boardInput" element={<BoardInput />} />
-                <Route path="/boardPage" element={<BoardList />} />
+                <Route path="/boardInput" element={<BoardWritePage />} />
+                <Route path="/boardPage" element={<BoardListPage />} />
                 <Route path="/boardDetail" element={<BoardDetailPage />} />
                 <Route element={<AdminRoute />}>
                   <Route path="/admin" element={<MyPage />}>
@@ -158,36 +107,6 @@ function App() {
               <Route path="/*" element={<ErrorPage />} />
             </Routes>
           </BrowserRouter>
-          {/* 아래는 옛날방식인데 이러면 라우팅은 잘 되나, 로그인을 안했을 때와
-        토큰만료로 로그인을 다시해야하는 경우 로그인 페이지로 유도하지 못함
-        위의 방식을 어떻게든 수정해서 써야함 */}
-          {/* <BrowserRouter>
-          <Routes>
-            {checkLogin ? (
-              <>
-                <Route path="/home" element={<MainPage />} />
-                <Route path="/commu" element={<BoardPage />} />
-                <Route path="/time" element={<TimeTablePage />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/dm" element={<MessagePage />}>
-                  <Route path="/dm" element={<MListBox />} />
-                  <Route path="Read" element={<MRead />} />
-                </Route>
-                <Route path="/dm/send" element={<MessageWritePage />} />
-                <Route path="/boardInput" element={<BoardInput />} />
-                <Route path="/boardPage" element={<BoardList />} />
-                <Route path="/*" element={<ErrorPage />} />
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<SplashPage />} />
-                <Route path="/home" element={<LoginPage />} />
-                <Route path="/regist" element={<RegistPage />} />
-                <Route path="/*" element={<ErrorPage />} />
-              </>
-            )}
-          </Routes>
-        </BrowserRouter> */}
         </S.Frame>
       </ThemeProvider>
     </>
