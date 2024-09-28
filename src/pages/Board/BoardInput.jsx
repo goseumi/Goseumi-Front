58b3d951943c4098598668204f0fa5130522946e
yungan9 <geyun105@naver.com>
2024-09-29 06:53:33 +0900

    feat : BoardInput handleChange 함수 추가

--- src/pages/Board/BoardInput.jsx
+++ src/pages/Board/BoardInput.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import FormHeader from "../../components/common/FormHeader";
+import { useState } from "react";
 
 const StyledInput = styled.input`
     width: 90%;
@@ -33,16 +34,36 @@ const Container = styled.div`
 `;
 
 function BoardInput(){
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === 'title') {
+            setTitle(value);
+            console.log('제목:', value);
+        } else if (name === 'content') {
+            setContent(value);
+            console.log('내용:', value);
+        }
+      }
     return(<>
             <FormHeader text="글쓰기" type="input"/>
             <Container>
                 <StyledInput
                     type="text"
+                    name="title"
                     placeholder="제목"
+                    value={title}
+                    onChange={handleChange}
                 />
                 <StyledTextarea
+                    name="content"
                     placeholder="내용을 입력하세요"
                     rows="30"
+                    value={content}
+                    onChange={handleChange}
                 />
             </Container>
         </>
